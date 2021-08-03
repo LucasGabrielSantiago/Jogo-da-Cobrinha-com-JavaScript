82bbfb961a1a69bfe29a6832da47e584d7fa61fc
LucasGabrielSantiago <lucasgabrielsantiagoferreira@gmail.com>
2021-08-03 06:24:00 -0300

    v1

---
+++ s.js
@@ -0,0 +1,46 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
+}
+
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarjogo(){}
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakey = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead ={
+        x: snakeX,
+        y: SnakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarjogo, 100);
+criarBG();
+criarCobrinha();
